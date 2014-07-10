338b35071ffcdf66cdcad3e2ea71e496447cdf51
Douglas Riches <dougr@newyyz.com>
2014-07-10 10:15:09 -0400

    add simple model to the main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,9 @@
  */
 angular.module('webCalculatorApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.model = {
+      expression: [],
+      currentNumber: [],
+      hasDecimal: false
+    };
   });
